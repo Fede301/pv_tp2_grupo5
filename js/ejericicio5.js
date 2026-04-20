c7f46e6ea5529bf8b01f620380567fdf6498ae72
Fede301 <federicofof305@gmail.com>
2026-04-20 20:05:30 -0300

    ejercicio 5

---
+++ js/ejericicio5.js
@@ -0,0 +1,6 @@
+let horas = 10, minutos = 36, segundos;
+
+segundos = ((horas * 60) + minutos) * 60;
+
+console.log(`Hora: ${horas} | Minutos: ${minutos}`);
+console.log(`Conversion a segundos: ${segundos}`);
